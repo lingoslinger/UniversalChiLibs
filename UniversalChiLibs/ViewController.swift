79367db6c31d3b055eb490328e46aa25acab8784
Allan Evans <allan@agesoftwareconsultingllc.com>
2023-11-02 08:51:10 -0500

    Initial Commit

---
+++ UniversalChiLibs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversalChiLibs
+//
+//  Created by Allan Evans on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
