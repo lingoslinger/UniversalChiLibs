f2a702b92f1e609195c102385d1e60deb9f9d32b
Allan Evans <allan@agesoftwareconsultingllc.com>
2023-11-02 09:07:14 -0500

    added view controllers and stub code
    Signed-off-by: Allan Evans <allan@agesoftwareconsultingllc.com>

---
+++ UniversalChiLibs/LibraryDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LibraryDetailViewController.swift
+//  UniversalChiLibs
+//
+//  Created by Allan Evans on 11/2/23.
+//
+
+import UIKit
+
+class LibraryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
