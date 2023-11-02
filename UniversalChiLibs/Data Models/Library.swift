ab35f2e755f904e336d322d0c22208d19b37198c
Allan Evans <allan@agesoftwareconsultingllc.com>
2023-11-02 10:29:23 -0500

    enabled dialing in phone number field; some overdue data cleanup
    Signed-off-by: Allan Evans <allan@agesoftwareconsultingllc.com>

--- UniversalChiLibs/Data Models/Library.swift
+++ UniversalChiLibs/Data Models/Library.swift
@@ -11,26 +11,22 @@ import Foundation
 struct Library: Decodable {
     let address : String?
     let city : String?
-    let cybernavigator : String?
     let hoursOfOperation : String?
     let location : Location?
     let name : String?
     let phone : String?
     let state : String?
-    let teacherInTheLibrary : String?
     let website : Website?
     let zip : String?
     
     enum CodingKeys: String, CodingKey {
         case address = "address"
         case city = "city"
-        case cybernavigator = "cybernavigator"
         case hoursOfOperation = "hours_of_operation"
         case location = "location"
         case name = "name_"
         case phone = "phone"
         case state = "state"
-        case teacherInTheLibrary = "teacher_in_the_library"
         case website = "website"
         case zip = "zip"
     }
@@ -39,13 +35,11 @@ struct Library: Decodable {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         address = try values.decodeIfPresent(String.self, forKey: .address)
         city = try values.decodeIfPresent(String.self, forKey: .city)
-        cybernavigator = try values.decodeIfPresent(String.self, forKey: .cybernavigator)
         hoursOfOperation = try values.decodeIfPresent(String.self, forKey: .hoursOfOperation)
         location = try values.decodeIfPresent(Location.self, forKey: .location)
         name = try values.decodeIfPresent(String.self, forKey: .name)
         phone = try values.decodeIfPresent(String.self, forKey: .phone)
         state = try values.decodeIfPresent(String.self, forKey: .state)
-        teacherInTheLibrary = try values.decodeIfPresent(String.self, forKey: .teacherInTheLibrary)
         website = try values.decodeIfPresent(Website.self, forKey: .website)
         zip = try values.decodeIfPresent(String.self, forKey: .zip)
     }
