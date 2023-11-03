ab7311bae7864fbb829f9150816abdee87f424e3
Allan Evans <allan@agesoftwareconsultingllc.com>
2023-11-03 00:29:38 -0500

    updated data source method
    Signed-off-by: Allan Evans <allan@agesoftwareconsultingllc.com>

---
+++ UniversalChiLibs/Data Source and Data Models/LibraryDataSource.swift
@@ -0,0 +1,93 @@
+//
+//  LibraryDataSource.swift
+//  SwiftLibrariesNoSB
+//
+//  Created by Allan Evans on 9/28/21.
+//
+
+import Foundation
+import UIKit
+
+class LibraryDataSource: NSObject {
+    var libraryDictionary = Dictionary<String, [Library]>()
+    var sectionTitles: [String] {
+        get {
+            return libraryDictionary.keys.sorted()
+        }
+    }
+    
+    func getLibraryData(_ completion: @escaping (Error?) -> Void) {
+        if libraryDictionary.isEmpty {
+            newDataRequest(completion)
+        }
+    }
+    
+    private func newDataRequest(_ completion: @escaping (Error?) -> Void) {
+        guard let URL = URL(string: "https://data.cityofchicago.org/resource/x8fc-8rcq.json") else { return }
+        URLSession.shared.dataTask(with: URL) { data, response, error in
+            if (error == nil) {
+                let decoder = JSONDecoder()
+                guard let libraryArray = try? decoder.decode([Library].self, from: data!) else {
+                    fatalError("Unable to decode JSON library data")
+                }
+                self.setupSectionsWithLibraryArray(libraryArray)
+            }
+            DispatchQueue.main.async { completion(error) }
+        }.resume()
+    }
+    
+    private func setupSectionsWithLibraryArray(_ libraryArray: [Library]) {
+        for library in libraryArray {
+            let firstLetterOfName = String.init(library.name?.first ?? Character.init(""))
+            if (libraryDictionary[firstLetterOfName] == nil) {
+                let sectionArray = [Library]()
+                libraryDictionary[firstLetterOfName] = sectionArray
+            }
+            libraryDictionary[firstLetterOfName]?.append(library)
+        }
+    }
+    
+    func currentLibrary(_ indexPath: IndexPath) -> Library? {
+        let sectionTitle = sectionTitles[indexPath.section]
+        let sectionArray = libraryDictionary[sectionTitle]
+        return sectionArray?[indexPath.row]
+    }
+}
+
+extension LibraryDataSource: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionTitle = sectionTitles[section]
+        let sectionArray = libraryDictionary[sectionTitle]
+        return sectionArray?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sectionTitles
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LibraryTableViewCell", for: indexPath)
+        guard let library = currentLibrary(indexPath) else { return cell }
+        guard let libraryName = library.name else { return cell }
+        guard let libraryHours = library.hoursOfOperation else { return cell }
+        let hoursString = libraryHours.formattedHours
+        if hoursString.contains("improvements") {
+            cell.textLabel?.text = libraryName + " - closed for improvements"
+        } else {
+            cell.textLabel?.text = libraryName
+        }
+        return cell
+    }
+}
--- UniversalChiLibs/SwiftLibraries/LibraryTableViewController.swift
+++ UniversalChiLibs/SwiftLibraries/LibraryTableViewController.swift
@@ -14,83 +14,31 @@ class LibraryTableViewController: UITableViewController {
     var sectionDictionary = Dictionary<String, [Library]>()
     var sectionTitles = [String]()
     
+    let libraryDataSource = LibraryDataSource()
+    
     // MARK: - view lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        let completionHander : SessionCompletionHandler = {(data : Data?, response : URLResponse?, error : Error?) -> Void in
-            if (error == nil) {
-                let decoder = JSONDecoder()
-                guard let libraryArray = try? decoder.decode([Library].self, from: data!) else {
-                    fatalError("Unable to decode JSON library data")
-                }
-                self.libraryArray = libraryArray
-                DispatchQueue.main.async(execute: {
-                    self.setupSectionsWithLibraryArray()
-                    self.tableView.reloadData()
-                })
+        tableView.dataSource = libraryDataSource
+        tableView.delegate = self
+        
+        libraryDataSource.getLibraryData { error in
+            if error != nil {
+                self.showErrorDialogWithMessage(message: error?.localizedDescription ?? "Unknown network error")
             } else {
-                DispatchQueue.main.async {
-                    self.showErrorDialogWithMessage(message: error?.localizedDescription ?? "Unknown network error")
-                }
+                self.tableView.reloadData()
             }
         }
-        LibraryURLSession().sendRequest(completionHander)
     }
+}
 
-    // MARK: - UITableViewDataSource and UITableViewDelegate methods
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return sectionTitles.count
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let sectionTitle = self.sectionTitles[section]
-        let sectionArray = self.sectionDictionary[sectionTitle]
-        return sectionArray?.count ?? 0
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LibraryTableViewCell")
-        let sectionTitle = self.sectionTitles[indexPath.section]
-        let sectionArray = self.sectionDictionary[sectionTitle]
-        let library = sectionArray?[indexPath.row]
-        cell?.textLabel?.text = library?.name
-        return cell!
-    }
-    
-    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-        return index
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return self.sectionTitles[section]
-    }
-    
-    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return self.sectionTitles
-    }
-    
-    // MARK: - navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+extension LibraryTableViewController {
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "LibraryDetailViewController" {
-            guard let indexPath = self.tableView.indexPathForSelectedRow else { return }
+            guard let indexPath = self.tableView.indexPathForSelectedRow,
+                  let library = libraryDataSource.currentLibrary(indexPath) else { return }
             let detailViewController = segue.destination as! LibraryDetailViewController
-            let sectionTitle = self.sectionTitles[indexPath.section]
-            let sectionArray = self.sectionDictionary[sectionTitle]
-            detailViewController.detailLibrary = sectionArray?[indexPath.row]
+            detailViewController.detailLibrary = library
         }
-    }
-    
-    // MARK: - private methods
-    func setupSectionsWithLibraryArray() {
-        for library in libraryArray {
-            let firstLetterOfName = String.init(library.name?.first ?? Character.init(""))
-            if (sectionDictionary[firstLetterOfName] == nil) {
-                let sectionArray = [Library]()
-                sectionDictionary[firstLetterOfName] = sectionArray
-            }
-            sectionDictionary[firstLetterOfName]?.append(library)
-        }
-        let unsortedLetters = self.sectionDictionary.keys
-        sectionTitles = unsortedLetters.sorted()
     }
 }
