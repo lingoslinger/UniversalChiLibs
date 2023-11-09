49b5a5820634620948eaa99a578f1fc8c9500cd5
Allan Evans <lingoslinger@me.com>
2023-11-08 19:27:03 -0600

    Added Coordinator pattern-based target with test targets
    Signed-off-by: Allan Evans <lingoslinger@me.com>

---
+++ UniversalChiLibs/CoordinatedSwiftLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatedSwiftLibs
+//
+//  Created by Allan Evans on 11/8/23.
+//  Copyright © 2023 AGE Software Consulting, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
