543c44d4e3e7c8f67414b11511cc72cfd76c571a
Allan Evans <lingoslinger@me.com>
2024-04-25 08:44:58 -0400

    removed premature optimization; libraryView toggle enabled
    Signed-off-by: Allan Evans <lingoslinger@me.com>

--- UniversalChiLibs/SwiftUILibraries/Views/LibraryView.swift
+++ UniversalChiLibs/SwiftUILibraries/Views/LibraryView.swift
@@ -9,18 +9,92 @@ import SwiftUI
 
 enum DisplayType {
     case list
-    case closestLocation
+    case location
 }
 
 struct LibraryView: View {
+    @StateObject var dataStore = LibraryDataSource()
+    @State var searchText = ""
     @State var displayType = DisplayType.list
     
-    var body: some View {
+    var libraries: [Library] {
+        var returnLibraries = dataStore.libraries
         switch displayType {
             case .list:
-                LibraryAlphaNavigationView()
-            case .closestLocation:
-                Text("Location View goes here")
+                returnLibraries = dataStore.libraries.filter {
+                    searchText.count == 0 ? true : $0.name.lowercased().contains(searchText.lowercased())
+                }
+            case .location:
+                returnLibraries = dataStore.libraries // for now, do distance check here eventually
+        }
+        return returnLibraries
+    }
+    
+    var sectionTitles: [String] {
+        let firstLetters = libraries.map { $0.name.prefix(1) }
+        return Array(Set(firstLetters)).map { String($0) }.sorted()
+    }
+    
+    var showNoResultsMessage: Bool {
+        searchText.count > 0 && libraries.count == 0
+    }
+    
+    var body: some View {
+        HStack {
+            NavigationView {
+                switch displayType {
+                    case .list:
+                        List {
+                            ForEach(sectionTitles, id: \.self) { sectionTitle in
+                                Section(header: Text(sectionTitle)) {
+                                    let currentLibraries = libraries.filter { $0.name.hasPrefix(sectionTitle) }.sorted { $0.name < $1.name }
+                                    ForEach(currentLibraries, id: \.self) { library in
+                                        NavigationLink(destination: LibraryDetailView(library: library)) {
+                                            Text(library.name)
+                                        }
+                                    }
+                                }
+                            }
+                            if showNoResultsMessage {
+                                Text("No results found for \"\(searchText)\"")
+                                    .padding()
+                                    .foregroundColor(.secondary)
+                                    .listRowBackground(Color.clear)
+                            }
+                        }
+                        .searchable(text: $searchText,
+                                    placement: .navigationBarDrawer(displayMode: .always),
+                                    prompt: "Search by library name")
+                        .navigationBarTitle("Chicago Libraries")
+                        .navigationBarItems(trailing:
+                                                Button(action: {
+                                                    print("location button pressed")
+                            displayType = .location
+                                                }) {
+                                                    Image(systemName: "location")}
+                        )
+                    case .location:
+                        List {
+                            ForEach(libraries, id: \.self) { library in
+                                NavigationLink(destination: LibraryDetailView(library: library)) {
+                                    Text(library.name)
+                                }
+                               
+                            }
+                        }
+                        .searchable(text: $searchText,
+                                    placement: .navigationBarDrawer(displayMode: .always),
+                                    prompt: "Search by address or zip code")
+                        .navigationBarTitle("Chicago Libraries")
+                        .navigationBarItems(trailing:
+                                                Button(action: {
+                                                    print("list button pressed")
+                            displayType = .list
+                                                }) {
+                                                    Image(systemName: "text.justify")}
+                        )
+                }
+            }
         }
     }
 }
