83585a0f7e2ad8ca3558c95e53085f00d3179fee
Allan Evans <lingoslinger@me.com>
2024-05-24 11:37:41 -0400

    a bit of refactoring in the distance sorted item
    Signed-off-by: Allan Evans <lingoslinger@me.com>

--- UniversalChiLibs/SwiftUILibraries/Views/LibraryItemDistanceSorted.swift
+++ UniversalChiLibs/SwiftUILibraries/Views/LibraryItemDistanceSorted.swift
@@ -28,17 +28,20 @@ struct LibraryItemDistanceSorted: View {
         }
     }
     
-    var body: some View {
+    var formattedDistance: String {
         let divisor = (metric ? 1000.0 : 1609.344)
         let unit = (metric ? "km" : "mi")
         
-        let miles = library.walkingDistance / divisor
-        let formattedDistance = miles.formatted(
+        let dist = library.walkingDistance / divisor
+        let formattedDistance = dist.formatted(
             .number
-                .rounded(rule: .up, increment: 0.1)
-        )
+                .rounded(rule: .up, increment: 0.1))
+        return "\(formattedDistance) \(unit)"
+    }
+    
+    var body: some View {
         NavigationLink(destination: LibraryDetailView(library: library)) {
-            Text("\(library.name)\n\(formattedDistance) \(unit)")
+            Text("\(library.name)\n\(formattedDistance)")
         }
     }
     
